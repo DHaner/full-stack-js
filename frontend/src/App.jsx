e9361ff11ec58773dc45bfc9283e27f74fd8163e
Daniel <daniel_oswaldo01@hotmail.com>
2025-09-19 15:36:32 -0600

    frontend created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AuthLayout from './layout/AuthLayout'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
+import ConfirmAccount from './pages/ConfirmAccount'
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='forgot-password' element={<ForgotPassword />} />
+          <Route path='confirm/:id' element={<ConfirmAccount />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -0,0 +1,8 @@
+
+export default function ConfirmAccount() {
+  return (
+    <>
+      <h1>Confirm Account Page</h1>
+    </>
+  );
+}
