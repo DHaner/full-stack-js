6a801f1208621e8d189e555b8b2be2cdbbff8c48
Daniel <daniel_oswaldo01@hotmail.com>
2025-10-06 23:26:44 -0600

    fix frontend patients provider

--- frontend/src/context/PatientsProvider.jsx
+++ frontend/src/context/PatientsProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import patientsService from "../services/PatientsService";
+import patientsService from "../services/patientsService";
 import useAuth from "../hooks/useAuth";
 
 const PatientsContext = createContext();
